283fd6c3d7e0e212ca0022622bc9afd90a8e564e
Isaac Cheng <isaaccheng9@gmail.com>
2021-03-24 01:59:54 +0000

    Implement reading of the file

--- ordering.cpp
+++ ordering.cpp
@@ -4,9 +4,28 @@
 #include "sales_order.cpp"
 #include "end_of_day.cpp"
 
-void read_input_file(string filename)
+void read_input_file(string file_name)
 {
-    ifstream input_file(filename);
+    string buffer;
+    ifstream file;
+    /* Throws an exception when EOF is reached. */
+    file.exceptions(ifstream::eofbit);
+
+    /* Opens the input file. */
+    try
+    {
+        file.open(file_name);
+    }
+    catch (ifstream::failure e)
+    {
+        cerr << "Exception: " << e.what() << endl;
+    }
+
+    /* Gets each line in the file. */
+    while (getline(file, buffer))
+    {
+        cout << buffer << endl;
+    }
 }
 
 int main(int argc, char **argv)
