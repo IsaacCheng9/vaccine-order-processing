7fe460efe9f4bdc437a851879a1598ab61c42be1
Isaac Cheng <isaaccheng9@gmail.com>
2021-03-24 00:47:04 +0000

    Add constructors for new customer and new sale

--- ordering.cpp
+++ ordering.cpp
@@ -10,22 +10,60 @@ using namespace std;
 class NewCustomerRecord
 {
     char record_type = 'C';
+    /* A four digit, zero-padded integer. */
     int customer_number;
     string customer_name;
+
+public:
+    NewCustomerRecord(int customer_number_input, string customer_name_input);
 };
+
+NewCustomerRecord::NewCustomerRecord(int customer_number_input,
+                                     string customer_name_input)
+{
+    cout << "OP: customer " << customer_number_input << "added";
+}
 
 class SalesOrderRecord
 {
     char record_type = 'S';
+    /* In the format YYYYMMDD. */
     int order_date;
+    /* 'N' for a normal order, 'X' for an express order. */
     char order_type;
+    /* Customer number of the customer raising the order. */
     int order_customer_number;
+    /* A three-digit, zero-padded integer. */
     int order_quantity;
+
+public:
+    SalesOrderRecord(int order_date_input, char order_type_input,
+                     int order_customer_number_input, int order_quantity_input);
 };
+
+SalesOrderRecord::SalesOrderRecord(int order_date_input, char order_type_input,
+                                   int order_customer_number_input,
+                                   int order_quantity_input)
+{
+    /* Sets order type name according to whether 'N' or 'E' was provided. */
+    string order_type_name;
+    if (order_type_input == 'N')
+    {
+        order_type_name = "normal";
+    }
+    else if (order_type_input == 'E')
+    {
+        order_type_name = "EXPRESS";
+    }
+
+    cout << "OP: customer " << order_customer_number_input << ": " <<
+    order_type_name << " order: quantity " << order_quantity_input;
+}
 
 class EndOfDayRecord
 {
     char record_type = 'E';
+    /* The date for which this is the end of the day, in the format YYYYMMDD. */
     int end_of_day;
 };
 
