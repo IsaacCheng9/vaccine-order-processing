dd20305440f46e17c8451226fc967fbc44bb414b
Isaac Cheng <isaaccheng9@gmail.com>
2021-03-25 01:06:08 +0000

    Split file up in header file

---
+++ ordering.hpp
@@ -0,0 +1,18 @@
+#ifndef __ORDERING_HPP
+#define __ORDERING_HPP
+
+#include <fstream>
+#include <iomanip>
+#include <vector>
+// TODO: Change to include .hpp files instead.
+#include "customer.cpp"
+#include "sales_order.cpp"
+using namespace std;
+
+void validate_parameters(int);
+bool process_new_order(SalesOrder *, vector<Customer *> &);
+void ship_pending_orders(vector<Customer *> &);
+void process_input_file(string, vector<Customer *> &);
+void free_allocated_memory(vector<Customer *> &);
+
+#endif
