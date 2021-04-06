e23dec653a21f5a417e3d9bd87c3acca002478d9
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-06 20:14:36 +0100

    Rename function to validate input line

--- sales_order.cpp
+++ sales_order.cpp
@@ -1,48 +1,45 @@
 #include "sales_order.hpp"
 
 /* Checks that the input line is valid for a sales order. */
-void validate_input_line(string input_line)
+void validate_input_order(string input_line)
 {
     bool valid = true;
 
     /* Sales order input line has a fixed length of 17 characters. */
     if (input_line.length() != 17)
     {
-        cerr << "This line in the input file does not have the correct format!"
+        cerr << "An input line for a sales order should only contain 17 "
+                "characters!"
              << endl;
         valid = false;
     }
-
     /* Order date should be a number; checks that columns 2-9 only contain
        digits. */
     for (int i = 1; i <= 8; i++)
     {
         if (isdigit(input_line[i]) == 0)
         {
-            cerr << "The order date should only contain digits!" << endl;
+            cerr << "Order date should only contain digits!" << endl;
             valid = false;
         }
     }
-
     /* Order type should be either 'N' or 'X'. */
     if (input_line[9] != 'N' && input_line[9] != 'X')
     {
-        cerr << "The order type should be either 'N' or 'X'!" << endl;
+        cerr << "Order type should be either 'N' or 'X'!" << endl;
         valid = false;
     }
-
     /* Order customer number should be a number; checks that columns 11-14 only
        contain digits. */
     for (int i = 10; i <= 13; i++)
     {
         if (isdigit(input_line[i]) == 0)
         {
-            cerr << "The order customer number should only contain digits!"
+            cerr << "Order customer number should only contain digits!"
                  << endl;
             valid = false;
         }
     }
-
     /* Order quantity should be a number; checks that columns 15-17 only
        contain digits. */
     for (int i = 14; i <= 16; i++)
@@ -64,7 +61,7 @@ void validate_input_line(string input_line)
 
 SalesOrder::SalesOrder(string input_line)
 {
-    validate_input_line(input_line);
+    validate_input_order(input_line);
 
     /* Order date is contained on columns 2-9, order type is contained on
        column 10, order customer number is contained on columns 11-14, and
